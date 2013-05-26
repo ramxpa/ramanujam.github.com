5b628cfa5fe3e2393c666096e02c3828c2bbc47b
Ram Parthasarathy <rparthasarathy@monetate.com>
2013-05-26 12:06:04 +0530

    remove unwanted jqueryism

--- monetate/mtintel/opti.js
+++ monetate/mtintel/opti.js
@@ -45,16 +45,5 @@ optiIntel.insertDisplayPanel = function(experimentNames) {
 };
 
 optiIntel.init = function () {
-    if (window.jQuery === undefined) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js";
-        script.onload = script.onreadystatechange = function(){
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                optiIntel.detectOptiExperiments();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }
+    optiIntel.detectOptiExperiments();
 }();
