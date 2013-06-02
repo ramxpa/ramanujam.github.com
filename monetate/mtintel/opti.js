34d30a9b69d70f1b123abdaa1cbcaa8de24a68c3
Ram Parthasarathy <rparthasarathy@monetate.com>
2013-06-02 06:42:12 -0400

    Fix styling

--- monetate/mtintel/opti.js
+++ monetate/mtintel/opti.js
@@ -51,7 +51,7 @@ optiIntel.insertDisplayPanel = function(experimentNames) {
         var containerDiv = document.createElement('div');
         bodyTag.appendChild(containerDiv);
         containerDiv.id = 'mtintel-container';
-        containerDiv.innerHTML = '<div id="mtintel-wrapper"><div class="navbar"><div class="navbar-inner"></div><h2 class="heading">Optimizely Campaigns</h2></div><table class="table table-striped total-numbers"><thead><tr><th>Experiments</th><th>Variations</th><th>Active</th></tr></thead><tbody><tr><td class="total-exp"></td><td class="total-var"></td><td class="total-active"></td></tr></tbody></table><table class="table table-1 data-table"><thead><tr><th class="th-1">Experiment Name</th><th class="th-2">Variations</th><th class="th-3">Active</th></tr></thead><tbody><tr><td class="exp-name"></td><td class="var-name"></td><td class="isactive"></td></tr></tbody></table></div>';
+        containerDiv.innerHTML = '<div id="mtintel-wrapper"><div class="navbar"><div class="navbar-inner"></div><h2 class="heading">Optimizely Campaigns</h2></div><table class="table table-striped total-numbers"><thead><tr><th>Experiments</th><th>Variations</th><th>Active</th></tr></thead><tbody><tr><td class="total-exp"></td><td class="total-var"></td><td class="total-active"></td></tr></tbody></table><table class="table table-striped table-1 data-table"><thead><tr><th class="th-1">Experiment Name</th><th class="th-2">Variations</th><th class="th-3">Active</th></tr></thead><tbody><tr><td class="exp-name"></td><td class="var-name"></td><td class="isactive"></td></tr></tbody></table></div>';
     }
 };
 
