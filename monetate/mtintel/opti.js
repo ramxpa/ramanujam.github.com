c732ca66e849a10a55ce50a4938334a837e8228c
Ram Parthasarathy <rparthasarathy@monetate.com>
2013-06-02 12:12:59 -0400

    Add temp experiment data

--- monetate/mtintel/opti.js
+++ monetate/mtintel/opti.js
@@ -17,17 +17,19 @@ optiIntel.detectOptiExperiments = function(){
         optiData.push(optiExp);
     });
 
-    var expCount = optiData.length;
+    optiData.totalExp = optiData.length;
+    optiData.totalVariations = Object.keys(allVariations).length;
+
     var activeCount = 0;
     for (var i = 0; i < optiData.length; i++) {
         if (optiData[i].enabled === 'true') {
-            activeCount = activeCount + 1;
+            activeCount++;
         }
     }
 
-    console.log('Opti Data --', optiData);
-    console.log('Exp count --', expCount);
-    console.log('Active count --', activeCount);
+    optiData.totalActive = activeCount;
+
+    console.log('optiData--', optiData);
 
     optiIntel.insertDisplayPanel(optiData);
     optiIntel.insertData(optiData);
@@ -51,16 +53,24 @@ optiIntel.insertDisplayPanel = function(experimentNames) {
         var containerDiv = document.createElement('div');
         bodyTag.appendChild(containerDiv);
         containerDiv.id = 'mtintel-container';
-        containerDiv.innerHTML = '<div id="mtintel-wrapper"><div class="navbar"><div class="navbar-inner"></div><h2 class="heading">Optimizely Campaigns</h2></div><table class="table table-striped total-numbers"><thead><tr><th>Experiments</th><th>Variations</th><th>Active</th></tr></thead><tbody><tr><td class="total-exp"></td><td class="total-var"></td><td class="total-active"></td></tr></tbody></table><table class="table table-bordered table-striped table-1 data-table"><thead><tr><th class="th-1">Experiment Name</th><th class="th-2">Variations</th><th class="th-3">Active</th></tr></thead><tbody><tr><td class="exp-name"></td><td class="var-name"></td><td class="isactive"></td></tr></tbody></table></div>';
+        containerDiv.innerHTML = '<div id="mtintel-wrapper"><div class="navbar"><div class="navbar-inner"></div><h2 class="heading">Optimizely Campaigns</h2></div><table class="table table-striped total-numbers"><thead><tr><th>Experiments</th><th>Variations</th><th>Active</th></tr></thead><tbody><tr><td class="total-exp"></td><td class="total-var"></td><td class="total-active"></td></tr></tbody></table><table class="table table-bordered table-striped table-1 data-table"><thead><tr><th class="th-1">Experiment Name</th><th class="th-2">Variations</th><th class="th-3">Active</th></tr></thead><tbody class="exp-data-table-body"></tbody></table></div>';
     }
 };
 
 
-optiIntel.insertData = function(campaignData) {
-    for (i = 0; i < campaignData.length; i ++) {
-        var expLi = document.createElement('li');
-        expLi.innerText = campaignData[i];
-        // displayPanel.appendChild(expLi);
+optiIntel.insertData = function(optiData) {
+    $('.total-exp').html(optiData.totalExp); 
+    $('.total-var').html(optiData.totalVariations);
+    $('.total-active').html(optiData.totalActive);
+
+    var expDataTable = $('.exp-data-table-body');
+    for (var i =0; i < optiData.length; i++) {
+        var expRow = document.createElement('tr');
+        expRow.className = 'exp-data-row';
+        expRow.innerHTML= '<td class="exp-name">'+ optiData[i].name +'</td>'+
+                      '<td class="var-name">'+ optiData[i].name +'</td>'+
+                      '<td class="isactive">'+ optiData[i].enabled +'</td>';
+        expDataTable.append(expRow);
     }
 };
 
